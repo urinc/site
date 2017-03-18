08483115c30f8b11b0bc164d0fd07929da2370d1
urinc <uri@ukr.net>
2017-03-18 22:36:23 +0200

    18032017

--- src/app/Components/item/item.component.ts
+++ src/app/Components/item/item.component.ts
@@ -19,33 +19,46 @@ export class ItemComponent implements OnInit {
   expandablePart : string = '';
   cleanedBody: string ;
   splittedBody : string[];
-
-
+  articleBody : string= "... ";
+  visibility : boolean =  true;
+  top : boolean = false;
+  
+    
+marker = "none"
   ngOnInit() {
      this.initialBody = this.item.body;
      this.cleanedBody = this.sanitizeText(this.initialBody);
      this.splittedBody = this.cleanedBody.split(' ');
-
-     this.getIntro()
+     this.getIntro();
+     this.getArticleBody();
+    
   }
 
 
 sanitizeText(body){
     let regex =/(&nbsp;|&mdash;|&ndash;|<([^>]+)>)/ig;
     return body.replace(regex, '')
-    .replace(/(&laquo;)/, '"')
-    .replace(/(&raquo;)/, ''); 
+    .replace(/(&laquo;)/ig, '"')
+    .replace(/(&raquo;)/ig, '"'); 
 }
 
 
  getIntro(){
    for (let i = 0; i < this.splittedBody.length ; i++){
      if (this.intro.length < 195) {
-       this.intro = this.intro + this.splittedBody[i] + ' ';
+       this.intro = this.intro + this.splittedBody.shift() + ' ';
       }
       else    return ;
    }
  }
+ 
+ getArticleBody(){
+    for (let i = 0; i < this.splittedBody.length ; i++){
+      this.articleBody = this.articleBody + this.splittedBody[i] + ' ';
+    }
+
+ }
+
 
 
   navigateTo() {
@@ -54,6 +67,14 @@ sanitizeText(body){
 
    approximateReading() {
     return Math.ceil(this.splittedBody.length / 150)
+  }
+
+  
+  
+  toggleVisibility(){
+    this.visibility=!this.visibility;
+    
+ 
   }
 
 }
