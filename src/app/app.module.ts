15e1bc689ffdbc7971aa9389fce1d7402b64073b
urinc <uri@ukr.net>
2017-03-21 00:24:15 +0200

    210817 hash strategy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -53,7 +53,7 @@ const appRoutes: Routes =[
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes),
+    RouterModule.forRoot(appRoutes, {useHash: true}),
     FormsModule,
     ReactiveFormsModule,
     HttpModule,
